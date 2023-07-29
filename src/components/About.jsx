7ce38d4cd91d19acb7bf58bd96b40bcd2567e5ac
Raihan Mahmud <raihan_uits@outlook.com>
2023-07-30 02:19:27 +0600

    rendering about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,13 @@
 export const About = () => {
-    return 'About'
+    return (
+        <div className="about">
+            <div className="text">
+                <h1>Little Lemon Restaurant</h1>
+                <h2>Mirpur, Dhaka</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, tempore! Ad molestiae possimus quasi quidem dolorum iusto ipsum quia obcaecati dolorem ab inventore necessitatibus est vero blanditiis fugit optio magni quae natus nobis vel sint assumenda, officiis numquam. Quisquam quae fugit asperiores iste quod! Ipsa laudantium harum porro rerum odit.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, tempore! Ad molestiae possimus quasi quidem dolorum iusto ipsum quia obcaecati dolorem ab inventore necessitatibus est vero blanditiis fugit optio magni quae natus nobis vel sint assumenda, officiis numquam. Quisquam quae fugit asperiores iste quod! Ipsa laudantium harum porro rerum odit.</p>
+            </div>
+            <img src="images/restaurant.jpg" alt="restaurant" />
+        </div>
+    )
 }
