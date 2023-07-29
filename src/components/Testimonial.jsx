14ea9fbd3276274cc41f35fb0f042ed49aed0a90
Raihan Mahmud <raihan_uits@outlook.com>
2023-07-29 20:42:49 +0600

    styling testimonials

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,21 +2,27 @@ import { reviews } from "../resources/review"
 
 export const Testimonial = () => {
     return (
-        <>
+        <div className="testimonial">
             <h2>Testimonials</h2>
-            {reviews.map(data => <Reviews item={data} key={data.id} />)}
-        </>
+            <section>
+                {reviews.map(data => <Reviews item={data} key={data.id} />)}
+            </section>
+        </div>
     )
 }
 
 const Reviews = ({ item }) => {
     return (
-        <section>
-            <img />
-            <h4>{item.name}</h4>
-            <h5>{item.prof}</h5>
+        <div className="review">
+            <div className="rating">
+                <img src={item.img} />
+                <div>
+                    <h3>{item.name}</h3>
+                    <h4>{item.prof}</h4>
+                    <span>Ratting: {item.rating}</span>
+                </div>
+            </div>
             <p>{item.review}</p>
-            <span>{item.rating}</span>
-        </section>
+        </div>
     )
 }
--- src/resources/review.js
+++ src/resources/review.js
@@ -1,32 +1,32 @@
 export const reviews = [
     {
         id: 1,
-        img: 'images/',
+        img: 'https://mymodernmet.com/wp/wp-content/uploads/2019/09/100k-ai-faces-6.jpg',
         name: 'Soleman Khan',
-        prof: 'actor',
+        prof: 'Actor',
         review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, exercitationem.',
         rating: 5
     },
     {
         id: 2,
-        img: 'images/',
+        img: 'https://static.generated.photos/vue-static/face-generator/landing/wall/7.jpg',
         name: 'Din Bondhu Mitro',
-        prof: 'singer',
+        prof: 'Singer',
         review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, exercitationem.',
         rating: 4
     },
     {
         id: 3,
-        img: 'images/',
+        img: 'https://mymodernmet.com/wp/wp-content/uploads/2019/09/100k-ai-faces-4.jpg',
         name: 'Rudro Shodullah',
-        prof: 'cricker',
+        prof: 'Cricker',
         review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, exercitationem.',
         rating: 5
     },
     {
         id: 4,
-        img: 'images/',
-        name: 'ali Abdaal',
+        img: 'https://static.generated.photos/vue-static/home/feed/male.png',
+        name: 'Ali Abdaal',
         prof: 'Developer',
         review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, exercitationem.',
         rating: 4
