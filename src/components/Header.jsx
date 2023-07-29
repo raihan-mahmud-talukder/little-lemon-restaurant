e40ce761013c88a94e36d07554dff91a1f3db1fc
Raihan Mahmud <raihan_uits@outlook.com>
2023-07-30 03:59:06 +0600

    fine tuning

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
                 <p>
                     Lorem ipsum dolor necessitatibus libero numquam! Recusandae et voluptatibus officia, maxime minima ex odio doloremque nisi nobis quaerat tempore vero praesentium optio! Architecto, mollitia. Magnam, aut officia blanditiis eligendi odit pariatur cumque corrupti vitae odio modi commodi temporibus expedita! Hic dignissimos quod impedit minima odio tenetur dolor repudiandae dolore magni deleniti repellat, vitae deserunt placeat esse, voluptas neque? Nihil numquam incidunt optio nisi.
                 </p>
-                <button>Reserve Table</button>
+                <button><a href="/reserve">Reserve Table</a></button>
             </aside>
             <div className="image">
                 <img src="images/serve.jpg" alt="serving food" />
