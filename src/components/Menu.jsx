61a937483f95c4f1b52ef777af2059a1e32d1bcb
Raihan Mahmud <raihan_uits@outlook.com>
2023-07-29 23:05:47 +0600

    renderning menus

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,3 +1,26 @@
+import { menus } from "../resources/menu"
+
 export const Menu = () => {
-    return 'Menu'
+    return (
+        <div className="menu">
+            <h1>Our Menu</h1>
+            <figure>
+                {menus.map(data => <Menus item={data} key={data.id} />)}
+            </figure>
+        </div>
+    )
+}
+
+const Menus = ({ item }) => {
+    return (
+        <div className="menus">
+            <img src={item.img} alt={item.title} />
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+            <div className="order">
+                <span>{item.price} €</span>
+                <button>Order Now</button>
+            </div>
+        </div>
+    )
 }
---
+++ src/resources/menu.js
@@ -0,0 +1,74 @@
+export const menus = [
+    {
+        id: 1,
+        title: 'Focaccia',
+        img: 'images/focaccia.jpg',
+        price: 16,
+        trending: false,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, ipsa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, exercitationem.'
+    },
+    {
+        id: 2,
+        title: 'Bruschetta',
+        img: 'images/bruchetta.webp',
+        price: 16,
+        trending: true,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, ipsa. Architecto, exercitationem.'
+    },
+    {
+        id: 3,
+        title: 'Margherita',
+        img: 'images/margherita.jpg',
+        price: 10,
+        trending: false,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, ipsa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, exercitationem.'
+    },
+    {
+        id: 4,
+        title: 'Spinaci',
+        img: 'images/spinaci.jpg',
+        price: 12,
+        trending: false,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, ipsa. Architecto, exercitationem.'
+    },
+    {
+        id: 5,
+        title: 'Greek Salad',
+        img: 'images/greek salad.jpg',
+        price: 15,
+        trending: true,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, exercitationem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, ipsa.'
+    },
+    {
+        id: 6,
+        title: 'Funghi',
+        img: 'images/funghi.jpg',
+        price: 12,
+        trending: false,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, ipsa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, exercitationem.'
+    },
+    {
+        id: 7,
+        title: 'Salamino',
+        img: 'images/salamino.jpg',
+        price: 15,
+        trending: false,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, ipsa. Architecto, exercitationem.'
+    },
+    {
+        id: 8,
+        title: 'Lemon Dessert',
+        img: 'images/lemon dessert.jpg',
+        price: 18,
+        trending: true,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, exercitationem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, ipsa.'
+    },
+    {
+        id: 9,
+        title: 'Prosciutto',
+        img: 'images/prosciutto.jpg',
+        price: 18,
+        trending: false,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, exercitationem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, ipsa.'
+    },
+]
